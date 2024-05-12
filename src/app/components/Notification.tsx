b1cf72bf3816f7e01d89396f8d4af7d82446e0dc
Ssewannonda Keith Edwin <skeith696@gmail.com>
2024-05-12 22:41:08 +0300

    style: add notification section

--- src/app/components/Notification.tsx
+++ src/app/components/Notification.tsx
@@ -2,7 +2,9 @@ import React from 'react';
 
 const Notification = () => {
     return ( 
-        <div>Notification</div>
+        <div className='h-12 bg-blue-700 text-white px-4 flex items-center justify-center text-center text-sm md:text-basse cursor-pointer'>
+            Free delivery for all orders above 20000shs. Order your food now!
+        </div>
      );
 }
  
