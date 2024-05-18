064a2721ac8881bfa2622db1ce14d0960238bd7c
Ssewannonda Keith Edwin <skeith696@gmail.com>
2024-05-18 13:51:59 +0300

    style: make slide content dynamic

--- src/app/components/home/Slider.tsx
+++ src/app/components/home/Slider.tsx
@@ -1,16 +1,45 @@
-import Image from 'next/image';
-import React from 'react';
+"use client"
 
-const Slider = () => {
-    const slides = []
+import Image from 'next/image';
+import React, { useEffect, useState} from 'react';
+
+const slides = [
+    {
+        id: "33508f2e-5f18-43df-b503-1ec0c22b42ad",
+        title: "always fresh and always crispy and always hot",
+        image: "/slide1.png"
+    },
+    {
+        id: "c299cf06-3f71-4bf6-a699-f4fd81997a16",
+        title: "we deliver your order wherever you are in Kampala",
+        image: "/slide2.png"
+    },
+    {
+        id: "510ac943-6d49-4bb7-b154-c2983c11fc67",
+        title: "the best pizza to share with your family",
+        image: "/slide3.jpg"
+    }
+]
+
+const Slider = () => {    
+    const [currentSlide, setCurrentSlide] = useState(0);
+
+    useEffect(() => {
+        const interval = setInterval(() => 
+            setCurrentSlide((previous) => (previous === slides.length - 1 ? 0 : previous + 1)), 2000)
+        return () => clearInterval(interval)
+    })
+
     return ( 
         <div className='flex flex-col h-[calc(100vh-6rem)] md:h-[calc(100vh-9rem)]'>
             <div className='h-1/2 flex items-center justify-center flex-col gap-8 text-red-500 font-bold'>
-                <h1 className='text-5xl md:text-6xl xl:text-7xl text-center uppercase p-4'>Text</h1>
+                <h1 className='text-5xl md:text-6xl xl:text-7xl text-center uppercase p-4 md:p-10'>
+                    {slides[currentSlide].title}
+                </h1>
                 <button className='bg-green-500 text-white py-4 px-8 rounded-md'>Order now</button>
             </div>
             <div className='w-full h-1/2 relative'>
-                <Image src={"/slide1.png"} alt='order now' fill className='object-cover'/>
+                <Image src={slides[currentSlide].image} alt='order now' fill className='object-cover'/>
             </div>
         </div>
      );
