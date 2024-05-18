af1a03a7620ae3ce83402cf1dfaba6e086f5a63d
Ssewannonda Keith Edwin <skeith696@gmail.com>
2024-05-18 16:16:48 +0300

    style: finalise featured section

--- src/app/components/home/Featured.tsx
+++ src/app/components/home/Featured.tsx
@@ -1,5 +1,6 @@
 import Image from 'next/image';
 import React from 'react';
+import { featuredProducts } from '../../../../data';
 
 const Featured = () => {
     return ( 
@@ -7,24 +8,27 @@ const Featured = () => {
             {/* wrapper */}
             <div className='w-max flex'>
                 {/* single item */}
-                <div className='w-screen h-[60vh] flex flex-col items-center justify-around p-4'>
-                    {/* image container */}
-                    <div className='relative flex-1 w-full'>
-                        <Image src={"/temporary/p1.png"} alt='' fill className=' object-contain'/>
-                    </div>
-                    {/* text container */}
-                    <div className=' flex-1 flex flex-col gap-4'>
-                        <h1 className='text-xl font-bold uppercase'>Title</h1>
-                        <p className=''>Desc</p>
-                        <span className='tex-xl font-bold'>shs 25000</span>
-                        <button className='bg-green-500 text-white p-3 rounded-md flex gap-2'>
-                        <span className=''>
-                            Add to cart
-                        </span>
-                        <Image src={"/add_to_cart.png"} alt='order now' width={20} height={20} className=''/>
-                        </button>
-                    </div>
-                </div>
+                {   
+                    featuredProducts.map(product => (
+                    <div key={product.id} className='w-screen h-[60vh] flex flex-col items-center text-center justify-around p-4 hover:bg-gray-100 trnasition-all duration-300 md:w-[50vw] xl:w-[33vw] xl:h-[90vh]'>
+                        {/* image container */}
+                        {product.img && <div className='relative flex-1 w-full hover:rotate-[60deg] transition-all duration-500'>
+                            <Image src={product.img} alt='' fill className=' object-contain'/>
+                        </div>}
+                        {/* text container */}
+                        <div className=' flex-1 flex flex-col items-center justify-center gap-4'>
+                            <h1 className='text-xl font-bold uppercase xl:text-2xl 2xl:text-3xl'>{product.title}</h1>
+                            <p className='p-4 2xl:p-8'>{product.desc}</p>
+                            <span className='tex-xl font-bold'>shs {product.price}</span>
+                            <button className='bg-green-500 text-white p-3 rounded-md flex gap-2'>
+                            <span className=''>
+                                Add to cart
+                            </span>
+                            <Image src={"/add_to_cart.png"} alt='order now' width={20} height={20} className=''/>
+                            </button>
+                        </div>
+                    </div>))
+                }
             </div>
         </div>
      );
