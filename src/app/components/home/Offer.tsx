7c5584a548b2aaed92a3b896c07ad776891379bc
Ssewannonda Keith Edwin <skeith696@gmail.com>
2024-05-18 12:54:34 +0300

    style: initial layout of home page

---
+++ src/app/components/home/Offer.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Offer = () => {
+    return ( 
+        <div>offer</div>
+     );
+}
+ 
+export default Offer;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,14 @@
 import Image from "next/image";
+import Slider from "./components/home/Slider";
+import Featured from "./components/home/Featured";
+import Offer from "./components/home/Offer";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      Ske Restaurant app
+      <Slider />
+      <Featured />
+      <Offer />
     </main>
   );
 }
