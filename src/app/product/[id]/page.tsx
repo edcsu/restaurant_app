7e1051cde77e87d297a92bf7d3e27fb944a5c849
Ssewannonda Keith Edwin <skeith696@gmail.com>
2024-05-19 12:56:53 +0300

    style: add single product structure

--- src/app/product/[id]/page.tsx
+++ src/app/product/[id]/page.tsx
@@ -1,8 +1,23 @@
 import React from 'react';
+import { singleProduct } from '../../../../data';
+import Image from 'next/image';
+import Price from '@/app/components/Price';
 
 const SingleProductPage = () => {
     return (
-        <div>SingleProduct</div>
+        <div className='p-4 lg:px-20 xl:px-40 h-screen flex flex-col justify-around text-orange-500 md:flex-row'>
+            {/* image */}
+            {singleProduct.img && 
+            (<div className='relative w-full h-1/2'>
+                <Image src={singleProduct.img} alt='product image' className='object-contain' fill />
+            </div>)}
+            {/* text */}
+            <div className='h-1/2 flex flex-col gap-4'>
+                <h1 className='text-3xl font-bold uppercase'>{singleProduct.title}</h1>
+                <p className=''>{singleProduct.desc}</p>
+                <Price price={singleProduct.price} id={singleProduct.id} options={singleProduct.options} />
+            </div>
+        </div>
     )
 }
 
