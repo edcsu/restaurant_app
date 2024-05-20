3ad4bb54328a5ba187bb802499bdd5f450d24efa
Ssewannonda Keith Edwin <skeith696@gmail.com>
2024-05-21 02:08:59 +0300

    style: make login page tablet responsive

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -6,13 +6,13 @@ const LoginPage = () => {
     return (
         <div className='p-4 h-[calc(100vh-6rem)] md:h-[calc(100vh-9rem)] flex items-center justify-center'>
             {/* Box */}
-            <div className='h-full shadow-xl rounded-md flex flex-col md:flex-row'>
+            <div className='h-full shadow-xl rounded-md flex flex-col md:flex-row md:h-[70%] md:w-full'>
                 {/* image container */}
-                <div className='relative h-1/3 w-full'>
+                <div className='relative h-1/3 w-full md:h-full md:w-1/2'>
                     <Image src={"/login.svg"} alt='login' fill className='object-cover'/>
                 </div>
                 {/* form container */}
-                <div className='p-10 flex flex-col gap-8'>
+                <div className='p-10 flex flex-col gap-8 md:w-1/2'>
                     <h1 className='font-bold text-xl'>Welcome</h1>
                     <p>
                         Log into your account or create a new one using the social buttons
